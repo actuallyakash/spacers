37018c5a498313b586ea046dc60f4bd765705cac
actuallyakash <akash_gupta@hotmail.com>
2021-06-16 23:40:30 +0530

    Added codepen link to play ▶

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -34,10 +34,10 @@ spacers({
     padding: false,
     margin: true,
     defaultMargin: {
-        'top': '20',
-        'bottom': '20',
-        'left': '20',
-        'right': '20'
+        'top': '10',
+        'bottom': '10',
+        'left': '10',
+        'right': '10'
     }
 });
 
